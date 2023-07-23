deba9d927db2ab3e574ac4773aedc411e677cf53
Hugo Sampaio <hugosampaio@rocketmail.com>
2023-07-23 11:43:35 -0300

    1st convolution version - paralel

--- hw/spinal/bpsk-scala/MyTopLevel.scala
+++ hw/spinal/bpsk-scala/MyTopLevel.scala
@@ -1,30 +1,53 @@
 package bpskscala
 
 import spinal.core._
+import spinal.lib._
+import spinal.lib.fsm._
 
 // Hardware definition
-case class MyTopLevel() extends Component {
+case class Convolution(width: Int) extends Component {
   val io = new Bundle {
-    val cond0 = in  Bool()
-    val cond1 = in  Bool()
-    val flag  = out Bool()
-    val state = out UInt(8 bits)
+    val signal    = slave Flow(SInt(16 bits))
+    val fir_coeff = in  Vec(SInt(16 bits), width)
+    val result =    master Flow(SInt(32 bits))
   }
+  val mul = Vec(Reg(SInt(32 bits)), width)
+  val signal_hist = Vec(Reg(SInt(16 bits)), width)
+  signal_hist.map(_ := 0)
 
-  val counter = Reg(UInt(8 bits)) init 0
+  val fsm = new StateMachine {
+    io.result.setIdle()
 
-  when(io.cond0) {
-    counter := counter + 1
-  }
+    val idle: State = new State with EntryPoint {
+      whenIsActive {
+        when(io.signal.valid) {
+          //shift
+          for (index <- width-1 to 1)
+            signal_hist(index) := signal_hist(index-1)
+          signal_hist(0) := io.signal.payload
 
-  io.state := counter
-  io.flag := (counter === 0) | io.cond1
+          //multiply
+          for( i <- 0 to width-1)
+            mul(i) := signal_hist(i) * io.fir_coeff(i)
+                goto(pushResult)
+        }
+      }
+    }
+
+    val pushResult: State = new State {
+      whenIsActive {
+        //sum
+        io.result.push(mul.reduceLeft(_ + _))
+        goto(idle)
+      }
+    }
+  }   
 }
 
 object MyTopLevelVerilog extends App {
-  Config.spinal.generateVerilog(MyTopLevel())
+  Config.spinal.generateVerilog(Convolution(43))
 }
 
 object MyTopLevelVhdl extends App {
-  Config.spinal.generateVhdl(MyTopLevel())
+  Config.spinal.generateVhdl(Convolution(43))
 }
--- hw/spinal/bpsk-scala/MyTopLevelFormal.scala
+++ hw/spinal/bpsk-scala/MyTopLevelFormal.scala
@@ -9,16 +9,6 @@ object MyTopLevelFormal extends App {
   FormalConfig
     .withBMC(10)
     .doVerify(new Component {
-      val dut = FormalDut(MyTopLevel())
-
-      // Ensure the formal test start with a reset
-      assumeInitial(clockDomain.isResetActive)
-
-      // Provide some stimulus
-      anyseq(dut.io.cond0)
-      anyseq(dut.io.cond1)
-
-      // Check the state initial value and increment
-      assert(dut.io.state === past(dut.io.state + U(dut.io.cond0)).init(0))
+      val dut = FormalDut(Convolution(43))
     })
 }
