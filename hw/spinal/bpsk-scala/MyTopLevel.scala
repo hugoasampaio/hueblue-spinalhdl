1b36dbb32f3a5bc9ad78c6a2e96bdaeb26b263f3
Hugo Sampaio <hugosampaio@rocketmail.com>
2023-07-29 10:28:14 -0300

    convolution + fir on top level, lacks tests

--- hw/spinal/bpsk-scala/MyTopLevel.scala
+++ hw/spinal/bpsk-scala/MyTopLevel.scala
@@ -29,7 +29,7 @@ case class Convolution(width: Int) extends Component {
           //multiply
           for( i <- 0 to width-1)
             mul(i) := signal_hist(i) * io.fir_coeff(i)
-                goto(pushResult)
+          goto(pushResult)
         }
       }
     }
@@ -41,13 +41,85 @@ case class Convolution(width: Int) extends Component {
         goto(idle)
       }
     }
-  }   
+  }
+}
+
+case class FirFilter() extends Component {
+  var coeff = Vec(SInt(16 bits), 43)
+    coeff(0) := 186
+    coeff(1) := 233
+    coeff(2) := 248
+    coeff(3) := 217
+    coeff(4) := 134
+    coeff(5) := 0
+    coeff(6) := -173
+    coeff(7) := -364
+    coeff(8) := -540
+    coeff(9) := -665
+    coeff(10) := -700
+    coeff(11) := -613
+    coeff(12) := -382
+    coeff(13) := 0
+    coeff(14) := 521
+    coeff(15) := 1151
+    coeff(16) := 1842
+    coeff(17) := 2533
+    coeff(18) := 3159
+    coeff(19) := 3660
+    coeff(20) := 3983
+    coeff(21) := 4095
+    coeff(22) := 3983
+    coeff(23) := 3660
+    coeff(24) := 3159
+    coeff(25) := 2533
+    coeff(26) := 1842
+    coeff(27) := 1151
+    coeff(28) := 521
+    coeff(29) := 0
+    coeff(30) := -382
+    coeff(31) := -613
+    coeff(32) := -700
+    coeff(33) := -665
+    coeff(34) := -540
+    coeff(35) := -364
+    coeff(36) := -173
+    coeff(37) := 0
+    coeff(38) := 134
+    coeff(39) := 217
+    coeff(40) := 248
+    coeff(41) := 233
+    coeff(42) := 186
+  
+  var filter = Convolution(43)
+  filter.io.fir_coeff := coeff
+  val counter = Reg(UInt(7 bits)) init (0)
+  val fsm = new StateMachine {
+
+    val pushInput: State = new State with EntryPoint {
+      whenIsActive {
+        if (counter == 0)
+          filter.io.signal.push(1)
+        else 
+          filter.io.signal.payload(0)
+        goto(pullResult)
+      }
+    }
+
+    val pullResult: State = new State {
+      whenIsActive {
+        val ans = filter.io.result.toReg()
+        println(ans)
+        counter := counter + 1
+        goto(pushInput)
+      }
+    }
+  }
 }
 
 object MyTopLevelVerilog extends App {
-  Config.spinal.generateVerilog(Convolution(43))
+  Config.spinal.generateVerilog(FirFilter())
 }
 
 object MyTopLevelVhdl extends App {
-  Config.spinal.generateVhdl(Convolution(43))
+  Config.spinal.generateVhdl(FirFilter())
 }
--- hw/spinal/bpsk-scala/MyTopLevelSim.scala
+++ hw/spinal/bpsk-scala/MyTopLevelSim.scala
@@ -2,12 +2,14 @@ package bpskscala
 
 import spinal.core._
 import spinal.core.sim._
+import spinal.lib._
+import spinal.lib.fsm._
+
 
 object MyTopLevelSim extends App {
-  Config.sim.compile(Convolution(43)).doSim { dut =>
+  
+  Config.sim.compile(FirFilter()).doSim { dut =>
     // Fork a process to generate the reset and the clock on the dut
     dut.clockDomain.forkStimulus(period = 10)
-    
-
   }
 }
